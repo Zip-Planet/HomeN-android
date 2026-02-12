25b4dab496a9aa4c8f3a0ee7fbab72831b24339e
soohwan-k <newpoo@naver.com>
2026-02-12 12:26:19 +0900

    feat: 상단 statusBar 침범 안하도록 수정

--- composeApp/src/commonMain/kotlin/com/devndev/homen/ui/component/HomeNScreen.kt
+++ composeApp/src/commonMain/kotlin/com/devndev/homen/ui/component/HomeNScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -25,7 +26,7 @@ fun HomeNScreen(
     content: @Composable (PaddingValues) -> Unit,
 ) {
     Scaffold(
-        modifier = modifier,
+        modifier = modifier.statusBarsPadding(),
         topBar = topBar,
         bottomBar = bottomBar,
         containerColor = containerColor,
