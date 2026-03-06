f15183754cd4601cd338c2aef662db6844b1353c
soohwan-k <newpoo@naver.com>
2026-03-06 10:50:03 +0900

    feat: Ktor Network 초기 설정

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -23,10 +24,23 @@ kotlin {
             implementation(project(":core:common"))
             implementation(libs.koin.core)
             implementation(libs.kotlinx.coroutines.core)
+            
+            // Ktor 공통 의존성
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.kotlinx.serialization.json)
         }
 
         androidMain.dependencies {
             implementation(libs.kakao.user.v2)
+            implementation(libs.ktor.client.okhttp)
+        }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
