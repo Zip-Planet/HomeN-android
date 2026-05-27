4e7c964e6c425be827c418653df75ce02c6bc171
soohwan-k <newpoo@naver.com>
2026-05-27 20:45:58 +0900

    fix: 토큰 리프레시 로직 수정

--- core/common/src/commonMain/kotlin/com/devndev/homen/core/common/Config.kt
+++ core/common/src/commonMain/kotlin/com/devndev/homen/core/common/Config.kt
@@ -4,8 +4,8 @@ package com.devndev.homen.core.common
  * 앱 전역 설정 및 상수
  */
 object Config {
-    const val BASE_URL = "http://43.201.71.104:8000/api/v1"
-    
+    //    const val BASE_URL = "http://43.201.71.104:8000/api/v1"
+    const val BASE_URL = "http://192.168.45.69:8080/api/v1"
     const val REQUEST_TIMEOUT = 5000L
     const val CONNECT_TIMEOUT = 5000L
     const val SOCKET_TIMEOUT = 30000L
